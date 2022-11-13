ac17ef721e6d637a7966a40f0938577637600e4d
cat dev <117660464+whynotlol1@users.noreply.github.com>
2022-11-13 13:47:15 +0300

    Update help_command.py

--- bot_package/help_command/help_command.py
+++ bot_package/help_command/help_command.py
@@ -1,4 +1,7 @@
 from bot_package.help_command import commands_list as cmd_list
+from bot_package.starter.initializer import bot
+from bot_package.starter import initializer
+from telebot import types
 
 separate = '-' * 30
 
@@ -11,3 +14,27 @@ def send_full_help_list() -> str:
             message_returned += f'{cmd_list.commands_list[command]["arguments"]}\n'
     message_returned += '\n\nSupport: https://discord.gg/23mrnTkTX4'
     return message_returned
+
+
+def bot_help_command(message):
+    bot.send_message(message.chat.id,
+                     f'Welcome to {initializer.bot_data["name"]}\nBot version: {initializer.bot_data["version"]}')
+    inline_keyboard = types.InlineKeyboardMarkup()
+    for command in cmd_list.commands_list.keys():
+        inline_keyboard.add(types.InlineKeyboardButton(text=f'/{command}', callback_data=command))
+    inline_keyboard.add(types.InlineKeyboardButton(text='Full help list', callback_data='full_help_list'))
+    bot.send_message(message.chat.id, 'What command do you need help with?', reply_markup=inline_keyboard)
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_query(call):
+    for command in cmd_list.commands_list.keys():
+        if call.data == command:
+            if cmd_list.commands_list[command]["arguments"] != 'none':
+                bot.send_message(call.message.chat.id,
+                                 f'/{command} :\n{cmd_list.commands_list[command]["description"]}\n{cmd_list.commands_list[command]["arguments"]}')
+            else:
+                bot.send_message(call.message.chat.id,
+                                 f'/{command} :\n{cmd_list.commands_list[command]["description"]}\n')
+    if call.data == 'full_help_list':
+        bot.send_message(call.message.chat.id, send_full_help_list())
