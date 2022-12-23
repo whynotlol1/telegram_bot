a79fcc804668c19dd2bc29dd94b60bd98eddbaa5
cat dev <117660464+whynotlol1@users.noreply.github.com>
2022-12-23 13:52:00 +0300

    Update game.py

--- bot_package/other_commands/game.py
+++ bot_package/other_commands/game.py
@@ -22,27 +22,28 @@ def game_realization(message):
     bot.register_next_step_handler(msg, guess_password)
 
 
-def wrong_password(message, text: str) -> None:
+def check(message):
     global correct_password
-    bot.send_message(message.chat.id, 'Wrong password, try again.')
-    message_text = 'There are some tips:\n'
-    for symbol in text:
-        if symbol in correct_password and text.index(symbol) == correct_password.index(symbol):
-            message_text += symbol
-        else:
-            message_text += '*'
-    if len(message_text) < 10:
-        message_text += '*' * (10 - len(message_text))
-    message_text += '\n- these are the symbols you guessed correctly.'
-    message_text += f'\n("*" - the symbols left to guess.)'
-    bot.send_message(message.chat.id, message_text)
-    message_text += f'\n{"-"*100}\nAnd these are the symbols you guessed, however they must be in the other places:\n'
-    for symbol in text:
-        if symbol in correct_password and text.index(symbol) != correct_password.index(symbol):
-            message_text += f'"{symbol}"; '
-    bot.send_message(message.chat.id, message_text)
-    msg = bot.send_message(message.chat.id, 'Try again:')
-    bot.register_next_step_handler(msg, guess_password)
+    password_guess = message.text[0:10]
+    print(password_guess)
+    if password_guess == correct_password:
+        bot.send_message(message.chat.id, 'You guessed the password!')
+        return
+    else:
+        bot.send_message(message.chat.id, 'You didn`t guess the password.')
+        message_text = ''
+        for symbol in password_guess:
+            if symbol in correct_password and password_guess.index(symbol) == correct_password.index(symbol):
+                message_text += symbol
+            else:
+                message_text += '*'
+        if len(message_text) < 10:
+            message_text += '*' * (10 - len(message_text))
+        if message_text != '**********':
+            message_text += '\n- these are the symbols you guessed correctly.'
+            bot.send_message(message.chat.id, message_text)
+        msg = bot.send_message(message.chat.id, 'Try again:')
+        bot.register_next_step_handler(msg, guess_password)
 
 
 def guess_password(message):
@@ -50,19 +51,5 @@ def guess_password(message):
     if message.text == 'stop':
         bot.send_message(message.chat.id, 'Stopping the game...')
         return
-    if len(message.text) == 10:
-        if message.text == correct_password:
-            bot.send_message(message.chat.id, 'You guessed the password!')
-            return
-        else:
-            wrong_password(message.text)
-    elif len(message.text) > 10:
-        if message.text[0:11] == correct_password:
-            bot.send_message(message.chat.id, 'You guessed the password!')
-            return
-        else:
-            wrong_password(message.text[0:11])
-
-        bot.send_message(message.chat.id, 'The password must contain 10 symbols.')
-        msg = bot.send_message(message.chat.id, 'Try again:')
-        bot.register_next_step_handler(msg, guess_password)
+    else:
+        check(message)
