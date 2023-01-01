936209c5e19748a79bb6db372ca9d7535211bb21
cat dev <117660464+whynotlol1@users.noreply.github.com>
2023-01-02 01:16:14 +0300

    Update main.py

--- bot_package/starter/main.py
+++ bot_package/starter/main.py
@@ -4,14 +4,17 @@ from bot_package.other_commands import translate_command
 from bot_package.other_commands import cprice_command
 from bot_package.help_command import help_command
 from bot_package.starter import initializer
+from telebot import types
 
 bot = initializer.bot
 
 
 @bot.message_handler(commands=['start'])
 def start_handler(message):
+    contact_keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+    contact_keyboard.add(types.KeyboardButton(text="Contacts"))
     bot.send_message(message.chat.id, 'Starting...')
-    bot.send_message(message.chat.id, f'Welcome to {initializer.bot_data["name"]}')
+    bot.send_message(message.chat.id, f'Welcome to {initializer.bot_data["name"]}', reply_markup=contact_keyboard)
 
 
 @bot.message_handler(commands=['help'])
@@ -37,6 +40,12 @@ def call_cprice_command(message):
 @bot.message_handler(commands=['translate'])
 def call_translate_command(message):
     translate_command.translate(message)
+
+
+@bot.message_handler(content_types=['text'])
+def text_handler(message):
+    if message.text == 'Contacts':
+        bot.send_message(message.chat.id, initializer.bot_data['contacts'])
 
 
 if __name__ == '__main__':
