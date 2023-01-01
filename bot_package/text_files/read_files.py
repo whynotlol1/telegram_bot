a8701a712575ed1c5ceb0816446113946b8190e7
cat dev <117660464+whynotlol1@users.noreply.github.com>
2023-01-01 17:25:05 +0300

    Update read_files.py

--- bot_package/text_files/read_files.py
+++ bot_package/text_files/read_files.py
@@ -1,3 +1,3 @@
-def read_file(filename: str) -> list:
+def read_first_line(filename: str) -> str:
     with open(f'bot_package//text_files//{filename}', 'r') as file:
-        return file.read_lines()
+        return file.readline()
