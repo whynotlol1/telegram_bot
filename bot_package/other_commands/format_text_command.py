fc06650de696085715883cb6088e4c3756ffbaac
cat dev <117660464+whynotlol1@users.noreply.github.com>
2023-01-02 18:47:53 +0300

    Update format_text_command.py

--- bot_package/other_commands/format_text_command.py
+++ bot_package/other_commands/format_text_command.py
@@ -1,5 +1,24 @@
 from bot_package.starter.initializer import bot
-from bot_package import extras
+
+
+def format_text(text: str) -> str:
+    symbols = [',', '!', '?', '.', ':', ';']
+    text_to_be_formatted = []
+    for i in text:
+        text_to_be_formatted.append(i)
+    text_returned = ''
+    for i in range(len(text_to_be_formatted)):
+        try:
+            if i == 0 or (text_to_be_formatted[i - 2] == '.' and text_to_be_formatted[i - 1] == ' '):
+                text_returned += text_to_be_formatted[i].capitalize()
+            elif i + 1 < len(text_to_be_formatted) and text_to_be_formatted[i] == ' ' and text_to_be_formatted[i + 1] in symbols:
+                text_returned += text_to_be_formatted[i + 1]
+                text_to_be_formatted[i + 1] = None
+            else:
+                text_returned += text_to_be_formatted[i]
+        except TypeError:
+            pass
+    return text_returned
 
 
 def text_formatter(message):
@@ -10,7 +29,7 @@ def text_formatter(message):
 def text_formatter_step_2(message):
     try:
         text = message.text
-        send = extras.format_text(text)
+        send = format_text(text)
         bot.send_message(message.chat.id, f'Result: {send}')
     except IndexError:
         bot.send_message(message.chat.id, 'Invalid argument. Please try again.')
