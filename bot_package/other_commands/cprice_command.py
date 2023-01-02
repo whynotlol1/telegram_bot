a975c7b151e6a5da4c6d0fd938c28ea986024c0e
cat dev <117660464+whynotlol1@users.noreply.github.com>
2023-01-02 18:45:19 +0300

    Update cprice_command.py

--- bot_package/other_commands/cprice_command.py
+++ bot_package/other_commands/cprice_command.py
@@ -3,6 +3,10 @@ import requests
 import json
 
 
+def to_fixed(num: float, digits: int) -> str:
+    return f"{num:.{digits}f}"
+
+
 def get_crypto_price(crypto: str) -> str:
     key = f"https://api.binance.com/api/v3/ticker/price?symbol={crypto.upper()}USDT"
     response = requests.get(key)
@@ -19,6 +23,6 @@ def crypto_price_step_2(message):
     try:
         crypto = message.text
         price = get_crypto_price(crypto)
-        bot.send_message(message.chat.id, f'Current price of {crypto}: ${extras.to_fixed(float(price), 2)}')
+        bot.send_message(message.chat.id, f'Current price of {crypto}: ${to_fixed(float(price), 2)}')
     except IndexError:
         bot.send_message(message.chat.id, 'Please enter a valid crypto name.')
