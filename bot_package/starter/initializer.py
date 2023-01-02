cd2ef950b5949f3a4aa7c5e25a9274fed164939b
cat dev <117660464+whynotlol1@users.noreply.github.com>
2023-01-02 11:49:48 +0300

    Update initializer.py

--- bot_package/starter/initializer.py
+++ bot_package/starter/initializer.py
@@ -1,12 +1,10 @@
-from bot_package.text_files.__read_files__ import read_all_lines as read_everything
 from bot_package.text_files.__read_files__ import read_first_line as read_token
 from telebot import TeleBot
 
 
 bot_data = {
     "version": "v1.3",
-    "name": "Telegram Utils Bot",
-    "contacts": read_everything("contacts.txt")
+    "name": "Telegram Utils Bot"
 }
 
 token = read_token("token.txt")
